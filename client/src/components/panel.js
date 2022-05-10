3d4b8b002e48db25c0537dacd4c60fc3c0422c3e
mehulagarwal0001 <84068131+mehulagarwal0001@users.noreply.github.com>
2022-05-10 21:56:44 +0530

    final

--- client/src/components/panel.js
+++ client/src/components/panel.js
@@ -5,6 +5,63 @@ import { useNavigate } from "react-router-dom";
 import '../panel.css'
 const Panel = () => {
     const  [ctc,setctc]=useState(0);
+      const score=   ( value)=>{
+   
+        if(value<=100000){
+          return 300;
+         }
+         else if(value<=300000){
+            return 350;
+         }
+         else if(value<=500000){
+            return 450;
+         }
+         else if(value<=600000){
+            return 500;
+         }
+         else if(value<=700000){
+             return 550;
+         }
+         else if(value<=1000000){
+             return 600;
+         }
+         else if(value<=1500000){
+           return 650;
+         }
+         else {
+             return 700;
+         }
+     
+     }
+
+     const limit=   ( value)=>{
+   
+        if(value<=300){
+          return 20000;
+         }
+         else if(value<=350){
+            return 80000;
+         }
+         else if(value<=450){
+            return 100000;
+         }
+         else if(value<=500){
+            return 150000;
+         }
+         else if(value<=550){
+             return 200000;
+         }
+         else if(value<=600){
+             return 300000;
+         }
+         else if(value<=650){
+           return 400000;
+         }
+         else {
+             return 500000;
+         }
+     
+     }
     const response=axios.get('/api/user/profile',{
         headers:{
             'Content-Type':'application/json',
@@ -12,7 +69,8 @@ const Panel = () => {
         }
     }).then((res,err)=>{
         const json=  res.data;
-         setctc(json.ctc);
+        const jt=score(json.ctc);
+         setctc(limit(jt));
     //  console.log(ctc)
 
     }).catch((err)=>{
